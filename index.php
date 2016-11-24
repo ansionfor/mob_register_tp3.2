aa29829357027bf52a31b88d064ba158fde5163d
ansionfor <77931774@qq.com>
2016-11-24 14:24:15 +0800

    update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 define('APP_DEBUG', true);//线下调试模式
-define('CSS','/register/Public/Home/css');
-define('JS','/register/Public/Home/js');
-define('IMG','/register/Public/Home/img');
+define('CSS','/Public/Home/css');
+define('JS','/Public/Home/js');
+define('IMG','/Public/Home/img');
 
 
 
-define('UED','/register/Public/Ueditor/full');
+define('UED','/Public/Ueditor/full');
 
 include './ThinkPHP/ThinkPHP.php';
 
